005646058b5ab07f20985164288d1a04d8fed043
AnasFn <anas000.busy@gmai.com>
2023-03-27 13:27:07 +0600

    2nd commit

--- app/src/main/java/com/example/tipjava/MainActivity.java
+++ app/src/main/java/com/example/tipjava/MainActivity.java
@@ -6,14 +6,17 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.tipjava.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     long i;
     float result;
+    float total;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,24 +30,41 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.cash);
-                mediaPlayer.start(); // no need to call prepare(); create() does that for you
+               if (!binding.editText.getText().toString().isEmpty() || binding.editText.getText().toString().length() >=1){
 
-               float bill= Long.parseLong(binding.editText.getText().toString()) ;
+                   float bill= Long.parseLong(binding.editText.getText().toString()) ;
 
-               if (binding.radio20.isChecked()){
-                   result= (bill * 20)/ 100;
-                   gotoResult(result);
 
-               } else if (binding.radio16.isChecked()) {
-                   result= (bill * 16)/ 100;
-                   gotoResult(result);
 
-               }else if (binding.rdaio10.isChecked()){
+                   if (binding.radio20.isChecked()){
+                       result= (bill * 20)/ 100;
+                       total=bill+result; MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.cash);
+//                       mediaPlayer.start();
+//
+                       gotoResult(total);
 
-                   result= (bill * 10)/ 100;;
-                   gotoResult(result);
-               }
+                   } else if (binding.radio15.isChecked()) {
+                       result= (bill * 15)/ 100;
+                       total=bill+result;
+//                       MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.cash);
+//                       mediaPlayer.start();
+                       gotoResult(total);
+
+                   }else if (binding.rdaio10.isChecked()){
+
+                       result= (bill * 10)/ 100;;
+                       total=bill+result;
+//                       MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.cash);
+//                       mediaPlayer.start();
+                       gotoResult(total);
+
+                   }
+                   else {
+                       Toast.makeText(MainActivity.this, "slect percent", Toast.LENGTH_SHORT).show();
+                   }
+               }else Toast.makeText(MainActivity.this, "amount is empty", Toast.LENGTH_SHORT).show();
+
+
 
 
             }
@@ -57,6 +77,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void gotoResult(float result){
+        MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.cash);
+        mediaPlayer.start();
         Intent intent= new Intent(MainActivity.this, ResultActivity.class);
         intent.putExtra("result", result);
         startActivity(intent);
